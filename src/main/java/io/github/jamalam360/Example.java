1216c218c9e1edefb18bd927c7a0b0502f367ca7
Jamalam360 <56727311+Jamalam360@users.noreply.github.com>
2021-08-30 23:58:38 +0100

    example

---
+++ src/main/java/io/github/jamalam360/Example.java
@@ -0,0 +1,12 @@
+package io.github.jamalam360;
+
+import java.io.IOException;
+
+/**
+ * @author Jamalam360
+ */
+public class Example {
+    public static void main(String[] args) throws IOException {
+        OurBallsHelper.executeBalls();
+    }
+}
